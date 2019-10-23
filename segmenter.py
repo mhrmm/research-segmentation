7b8ba1cf535ad9bffbd127fb3e41fc64bf60206d
Mark Hopkins <hopkinsm@reed.edu>
2019-10-23 09:08:23 -0700

    Refactored to use encoding.

---
+++ segmenter.py
@@ -0,0 +1,103 @@
+from readdata import read_test_data
+
+class XE:
+    X = 0
+    E = 1
+    
+    def encode(self, line):
+        sent = []
+        flags = []
+        line = line.strip() + " "
+        for i in range(len(line)):
+            if not line[i] == ' ':
+                if line[i + 1] == ' ':
+                    flags.append(XE.E)
+                else:
+                    flags.append(XE.X)
+                sent.append(line[i])
+        return sent, flags        
+
+    def decode(self, tokens, flags):
+        result = ""
+        for tok, flag in zip(tokens, flags):
+            result += tok
+            if flag == XE.E:
+                result += "  "
+        return result.strip()
+    
+    def domain_size(self):
+        return 2
+    
+    def terminator(self):
+        return XE.E
+
+    
+class BMES:
+    B = 0
+    M = 1
+    E = 2
+    S = 3
+
+    def encode(self, line):
+        sent = []
+        flags = []
+        line = line.strip() + " "
+        starting_new_word = True
+        for i in range(len(line)):
+            if not line[i] == ' ':
+                if line[i + 1] == ' ':
+                    if starting_new_word:
+                        flags.append(BMES.S)
+                    else:
+                        flags.append(BMES.E)
+                    starting_new_word = True
+                else:
+                    if starting_new_word:
+                        flags.append(BMES.B)
+                    else:
+                        flags.append(BMES.M)
+                    starting_new_word = False
+                sent.append(line[i])
+        return sent, flags        
+
+    def decode(self, tokens, flags):
+        result = ""
+        for tok, flag in zip(tokens, flags):
+            result += tok
+            if flag == BMES.E or flag == BMES.S:
+                result += "  "
+        return result.strip()
+    
+    def domain_size(self):
+        return 4
+
+    def terminator(self):
+        return XE.E
+
+
+
+def segment_sent(model, encoding, sent):
+    if len(sent) <= 1:
+        return sent
+    else:
+        flags = model(sent)
+        tokens = [tok for tok in sent]
+        assert len(tokens) == len(flags)
+        return encoding.decode(tokens, flags)
+       
+def segment_sents(model, lines):
+    result = ""
+    for sents in lines:
+        for sent in sents:
+            result += segment_sent(model, sent) + "  "
+        result = result.strip() + "\n"
+    return result.strip()
+
+def segment_file(model, input_file, output_file):
+    with open(input_file) as inhandle:
+        with open(output_file, 'w') as outhandle:
+            lines = read_test_data(inhandle, limit=13)
+            outhandle.write(segment_sents(model, lines))
+    
+
+            
